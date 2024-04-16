5a37b0233958e1f712334ac754a9b72d0e9d59d8
Tahiana <tahianarazakanome@gmail.com>
2024-04-17 00:35:35 +0300

    Ajout page jsf et backing bean liste compte bancaire

---
+++ src/main/java/mg/itu/tpbanqueismael/jsf/ListeComptes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanqueismael.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanqueismael.entity.CompteBancaire;
+import mg.itu.tpbanqueismael.service.GestionnaireCompte;
+
+/**
+ *
+ * @author ASPIRE
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+    
+    private List<CompteBancaire> accountList;
+    
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+    
+    public List<CompteBancaire> getAllComptes() {
+        if (accountList == null) {
+            accountList = gestionnaireCompte.getAllComptes();
+        }
+        return accountList;
+    }
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+}
