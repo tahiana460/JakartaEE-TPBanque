e4992ac61b4c87c038d85ed459c34aa7eb090075
Tahiana <tahianarazakanome@gmail.com>
2024-04-17 23:53:35 +0300

    Traitement erreurs et messages de succes

--- src/main/java/mg/itu/tpbanqueismael/jsf/transfertBean.java
+++ src/main/java/mg/itu/tpbanqueismael/jsf/transfertBean.java
@@ -9,18 +9,20 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import mg.itu.tpbanqueismael.entity.CompteBancaire;
 import mg.itu.tpbanqueismael.service.GestionnaireCompte;
+import mg.itu.tpbanqueismael.util.Util;
 
 /**
  * Backing bean de la page transfert.xhtml
+ *
  * @author ASPIRE
  */
 @Named(value = "transfertBean")
 @RequestScoped
 public class transfertBean {
-    
+
     @Inject
     private GestionnaireCompte gestionnaireCompte;
-    
+
     private CompteBancaire compteSource;
     private CompteBancaire compteDestination;
     private Long idSource;
@@ -50,19 +52,46 @@ public class transfertBean {
     public int getMontant() {
         return montant;
     }
-    
+
     public void loadCompteSource() {
         this.compteSource = gestionnaireCompte.findById(idSource);
     }
-    
+
     public void loadCompteDestination() {
         this.compteDestination = gestionnaireCompte.findById(idDestination);
     }
-    
+
     public String transferer() {
         this.loadCompteSource();
         this.loadCompteDestination();
+        
+        CompteBancaire source = gestionnaireCompte.findById(idSource);
+        CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+
+        boolean erreur = false;
+        if (source == null || destination == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            if(source == null) {
+                Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            }
+            if(destination == null) {
+                Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            }
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // à compléter pour le cas où le solde du compte source est insuffisant...
+                Util.messageErreur("Solde du compte source insuffisant !", "Solde du compte source insuffisant !", "form:source");
+                erreur = true;
+            }
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+
         gestionnaireCompte.transferer(compteSource, compteDestination, montant);
+        Util.addFlashInfoMessage("Transfert de "+ montant +" de "+ compteSource.getNom() +" vers "+ compteDestination.getNom() +" correctement effectué");
         return "listeComptes?faces-redirect=true";
     }
 
@@ -71,5 +100,5 @@ public class transfertBean {
      */
     public transfertBean() {
     }
-    
+
 }
